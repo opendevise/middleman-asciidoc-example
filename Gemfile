b4d31ac967df66d32117fc6a4ff011ac6c9a88f0
Dan Allen <dan.j.allen@gmail.com>
2023-10-31 03:25:23 -0600

    resolves #1 update to work with Middleman 4 and the AsciiDoc extension

--- Gemfile
+++ Gemfile
@@ -1,17 +1,20 @@
 source 'https://rubygems.org'
 
-gem 'middleman', '~> 3.3.7'
+gem 'middleman', '~> 4.0'
 
-# Live-reloading plugin
-gem 'middleman-livereload', '~> 3.4.2'
-
-gem 'slim', '~> 3.0.6'
+# Support templates written in Slim
+gem 'slim', '~> 4.1.0', require: false
 
 # Activate support for AsciiDoc
-gem 'asciidoctor', '~> 1.5.4'
+gem 'middleman-asciidoc', '~> 1.0'
+gem 'asciidoctor', '~> 2.0'
 gem 'coderay'
+
+# Live-reloading plugin
+gem 'middleman-livereload', '~> 3.4'
 
 # Faster file watching updates on Window
 gem 'wdm', '~> 0.1.0', :platforms => [:mswin, :mingw]
+
 # Windows does not come with time zone data
 gem 'tzinfo-data', :platforms => [:mswin, :mingw]
--- config.rb
+++ config.rb
@@ -1,101 +1,42 @@
-=begin
-# Use this extension to alter the base_dir value passed to Asciidoctor
-class AsciidoctorTweaks < Middleman::Extension
-  def initialize app, options_hash = {}, &block
-    super
-    app.before do
-      app.config[:asciidoc][:base_dir] = 'source/docs'
-      true
-    end
-  end
-end
+# Site structure
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+set :layouts_dir, 'layouts'
+set :build_dir, 'public'
 
-::Middleman::Extensions.register :asciidoctor_tweaks, AsciidoctorTweaks
+# Page-specific layouts
 
-activate :asciidoctor_tweaks
-=end
-
-# Reload the browser automatically whenever files change
-#activate :livereload
-
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
+# No layout
+# page "/path/to/file.html", layout: false
 #
-# With no layout
-# page "/path/to/file.html", :layout => false
+# Alternative layout
+# page "/path/to/file.html", layout: :otherlayout
 #
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
+# Set layout for pages by pattern
 # with_layout :admin do
 #   page "/admin/*"
 # end
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# Configure Slim
 
-###
-# Helpers
-###
+set :slim, format: :html, sort_attrs: false, pretty: true, disable_escape: true
+autoload :Asciidoctor, 'asciidoctor'
+require 'slim/include'
 
-set :asciidoc, {
+# Configure AsciiDoc extension
+
+activate :asciidoc, {
   safe: :safe,
-  template_dir: 'source/_templates',
-  attributes: %W(showtitle env=middleman env-middleman middleman-version=#{Middleman::VERSION})
+  template_dirs: 'source/_templates',
+  attributes: %W(idprefix=_ idseparator=- sectanchors=after icons=font source-highlighter=coderay coderay-css=style showtitle)
 }
-set :asciidoc_attributes, %w(icons=font source-highlighter=coderay coderay-css=style)
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
+# Configure URLs
 
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+activate :directory_indexes
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
-
-set :slim, { pretty: true, sort_attrs: false, disable_escape: true }
-
-# Build-specific configuration
-configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+# NOTE: The `server` command defaults to the :development environment.
+configure :development do
+  activate :livereload if ENV['LIVE_RELOAD'] == 'true'
 end
